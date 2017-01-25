d03bcd4a32d230e5904243112dbe31027aa35848
Mark Reichert <mark.a.reichert1@gmail.com>
2017-01-24 22:02:11 -0600

    implemented lucky number and random fortune and refresh button

--- main.py
+++ main.py
@@ -15,10 +15,34 @@
 # limitations under the License.
 #
 import webapp2
+import random
+
+def getRandomFortune():
+    # list of possible fortunes
+    fortunes = [
+        "I see much code in your future",
+        "Consider eating more fortune cookies",
+        "You have tamed the mighty Python, now you must free it onto the Great Spider's Web!"
+    ]
+
+    fortune_index = random.randint(0,2)
+    return fortunes[fortune_index]
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!!!!!!')
+        header = "<h1>Fortune Cookie</h1>"
+
+        fortune = "<strong>" + getRandomFortune() + "</strong>"
+        fortune_sentance = 'Your fortune: ' + fortune
+        fortune_paragraph = "<p>" + fortune_sentance + "</p>"
+
+        lucky_number = "<strong>" +  str(random.randint(1, 100)) + "</strong>"
+        number_sentance = 'Your lucky number: ' + lucky_number
+        number_paragraph = "<p>" + number_sentance + "</p>"
+
+        cookie_again_button = "<a href='.'><button>Another cookie plz!</button></a>"
+
+        self.response.write(header + fortune_paragraph + number_paragraph + cookie_again_button)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
